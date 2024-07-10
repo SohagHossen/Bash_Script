9e3fc87d0eb16aa1e6570c79d467277e881b0a1f
SohagHossen <sohaghossen.cse@gmail.com>
2024-07-10 11:51:28 +0600

    function using argument

---
+++ 13-functions-argument.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+#to access the argument when the script is run.
+echo "The First argument is $1"
+echo "the secont argument is $2"
+
+#print all argument in single print function
+
+echo "All the argument are -: $@"
+
+#print number of argument
+
+echo "Number of argument are-:$#"
